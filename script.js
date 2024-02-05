aa7f648e31ea9a73c2d5c50e4a7a03b8638ec3fa
AgustinGalieGR <agustingaliee@gmail.com>
2024-02-05 14:06:03 -0300

    Correccion reentrega

--- script.js
+++ script.js
@@ -1,94 +1,70 @@
 
-const Jigo1 = {nombre: 'Jigokuraku', volumen:1, editorial:'IVREA', demografia:'shonen', existencias:20, precio:500, img:"./Images/jigokuraku011.webp"};
-const Dandadan1 = {nombre: 'Dandadan', volumen:1,editorial:'IVREA', demografia:'shonen', existencias:20, precio:500, img:"./Images/dandadan011.webp"};
-const Vinland1 = {nombre: 'Vinland Saga', volumen:1,editorial:'OVNI', demografia:'seinen', existencias:20, precio:1000, img:"./Images/Vinland_Saga1.webp"};
-
-const catalogo = [Jigo1,Dandadan1,Vinland1];
+const Jigo1 = {nombre: 'Jigokuraku', volumen:1, editorial:'IVREA', demografia:'shonen', existencias:20, precio:500};
+const Dandadan1 = {nombre: 'Dandadan', volumen:1,editorial:'IVREA', demografia:'shonen', existencias:20, precio:500};
+const catalogo = [Jigo1,Dandadan1];
 let catalogoVisible = [];
 const contenidoCatalogo = document.getElementById("cajaProductos");
-/*catalogo.forEach(manga => {
-    const content = document.createElement("div");
-    content.innerHTML = `   
-    <img src="${manga.img}">
-    <h3>${manga.nombre}</h3>
-    <h4>Demografia:${manga.demografia}</h4>
-    <h4>Editorial:${manga.editorial}</h4>
-    <h5>Existencias:${manga.existencias}</h5>
-    <h3>Precio:${manga.precio}</h4>
-    <button>Comprar</button>
-    `;
-    contenidoCatalogo.append(content);
-});*/
-function mostrarProductosShonen(){
-    catalogoVisible.forEach(contenido => {
-        contenidoCatalogo.prepend(contenido);
-        catalogoVisible.pop();
-    });
-    let cant = 0;
-    catalogo.forEach((manga) => {
-        if(manga.demografia=='shonen'){
-            const content = document.createElement("div");
-            content.innerHTML = `   
-            <img src="${manga.img}">
-            <h3>${manga.nombre}</h3>
-            <h4>Demografia:${manga.demografia}</h4>
-            <h4>Editorial:${manga.editorial}</h4>
-            <h5>Existencias:${manga.existencias}</h5>
-            <h3>Precio:${manga.precio}</h4>
-            <button>Comprar</button>
-            `;
-            contenidoCatalogo.append(content);
-           // catalogoVisible.push(content);
-            cant++;  
-        };
-    });
-    if(cant==0){
-        alert("No hay ningun producto disponible");
+
+const addElement = () =>{
+
+    const nombre = prompt("Ingresar nombre del manga");
+    estandar(nombre);
+    const editorial = prompt("Ingresar nombre de la editorial");
+    estandar(editorial);
+    const demografia = prompt("Ingresar demografia");
+    estandar(demografia);
+    const existencias= prompt("Ingresar existencias");
+    const precio = prompt("Ingresar precio");
+
+    const mangaNuevo = {
+        nombre: nombre,
+        editorial: editorial,
+        demografia: demografia,
+        existencias: existencias,
+        precio: precio
     }
-    console.log(catalogoVisible);
-}
-function mostrarProductosSeinen(){
-    let cant = 0;
-    catalogo.forEach((manga) => {
-        if(manga.demografia=='seinen'){
-            const content = document.createElement("div");
-            content.innerHTML = `   
-            <img src="${manga.img}">
-            <h3>${manga.nombre}</h3>
-            <h4>Demografia:${manga.demografia}</h4>
-            <h3>Editorial:${manga.editorial}</h4>
-            <h5>Existencias:${manga.existencias}</h5>
-            <h4>Precio:${manga.precio}</h4>
-            <button>Comprar</button>
-            `;
-            contenidoCatalogo.append(content);  
-            cant++;
-        };
-    });
-    if(cant==0){
-        alert("No hay ningun producto disponible");
+    let pusheado = false;
+    let i = 0;
+    while(!pusheado){
+        if(catalogo[i]==null){
+            catalogo[i]=mangaNuevo;
+        }
+        pusheado = true;
     }
 }
-function mostrarProductosShojo(){
-    let cant = 0;
-    catalogo.forEach((manga) => {
-        if(manga.demografia=='shojo'){
-            const content = document.createElement("div");
-            content.innerHTML = `   
-            <img src="${manga.img}">
-            <h3>${manga.nombre}</h3>
-            <h4>Demografia:${manga.demografia}</h4>
-            <h3>Editorial:${manga.editorial}</h4>
-            <h5>Existencias:${manga.existencias}</h5>
-            <h4>Precio:${manga.precio}</h4>
-            <button>Comprar</button>
-            `;
-            contenidoCatalogo.append(content);  
-            cant++;
-        };
-    });
-    if(cant==0){
-        alert("No hay ningun producto disponible");
+
+const estandar = (x) =>{
+    if(x !=null){
+        x.toLowerCase(); 
+        x[0].toUpperCase();
     }
 }
-//Rever una forma en la que se vean todos los productos al abrir la pagina y que el filtro aplique hide a los productos que no cumplan con la condicion de filtrado
+
+const deleteElement = ()=> {
+    const buscar = prompt("Ingrese el nombre del manga a eliminar")
+    estandar(buscar);
+    let encontre = false;
+    let i = 0;
+    while(encontre==false){
+        if(buscar == catalogo[i].nombre){
+            catalogo[i] = null;
+            i++;
+            encontre = true;
+            for(j=i; j<catalogo.length;j++){
+                catalogo[j-1]==catalogo[j];
+            }
+            alert("El manga solicitado se ha eliminado correctamente")
+        }else{
+            i++;
+        }
+        
+    }
+    if(encontre==false){
+        alert("No se ha encontrado el manga solicitado");
+    }
+    const pregunta = confirm("Desea realizar otra busqueda para eliminar");
+    if(pregunta){
+        deleteElement();
+    }
+}
+console.log(catalogo);
