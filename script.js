664de807d708d6fe57bbb9a811c4db76e64145fd
AgustinGalieGR <agustingaliee@gmail.com>
2024-02-26 11:49:35 -0300

    Preentregable 2 hecho, falta carrito

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
-const Jigo1 = {nombre: 'JIGOKURAKU', volumen:1, editorial:'IVREA', demografia:'SHONEN', existencias:20, precio:500};
-const Dandadan1 = {nombre: 'DANDADAN', volumen:1,editorial:'IVREA', demografia:'SHONEN', existencias:20, precio:500};
-const catalogo = [Jigo1,Dandadan1];
+const Jigo1 = {nombre: 'JIGOKURAKU 1', editorial:'IVREA', demografia:'SHONEN', existencias:20, precio:500, img:"./Images/jigokuraku011.webp"};
+const Dandadan1 = {nombre: 'DANDADAN 1', editorial:'IVREA', demografia:'SHONEN', existencias:20, precio:500, img:"./Images/dandadan011.webp"};
+const VinlandSaga1 = {nombre: 'VINLAND SAGA 1', editorial:'OVNI', demografia:'SEINEN', existencias:50, precio:1000, img:"./Images/Vinland_Saga1.webp"};
+const NANA1 = {nombre: 'NANA', editorial:'IVREA', demografia:'SHOJO', existencias:50, precio:2000, img:"./Images/Nana1.webp"};
+const catalogo = [Jigo1,Dandadan1,VinlandSaga1,NANA1];
 let catalogoVisible = [];
 const contenidoCatalogo = document.getElementById("cajaProductos");
-
+clean();
+function mostrarCatalogo(){
+        catalogo.forEach((manga) => {
+        const content = document.createElement("div");
+        content.className+= "tarjeta";
+        content.innerHTML = `  
+        <img src="${manga.img}"> 
+        <h3>${manga.nombre}</h3>
+        <h4>Demografia:${manga.demografia}</h4>
+        <h3>Editorial:${manga.editorial}</h4>
+        <h5>Existencias:${manga.existencias}</h5>
+        <h4>Precio:${manga.precio}</h4>
+        <button>Comprar</button>
+        `;
+        contenidoCatalogo.append(content);  
+        catalogoVisible.push(content);
+    });
+}
 const addElement = () =>{
 
     const nombre = prompt("Ingresar nombre del manga");
@@ -60,11 +79,70 @@ const deleteElement = () =>{
         deleteElement();
     }
 }
+function mostrarProductosShonen(){
+    let cant = 0;
+    clean();
+    catalogo.forEach((manga) => {
+        if(manga.demografia=='SHONEN'){
+            const content = document.createElement("div");
+            content.className+= "tarjeta";
+            content.innerHTML = `
+            <img src="${manga.img}">   
+            <h3>${manga.nombre}</h3>
+            <h4>Demografia:${manga.demografia}</h4>
+            <h3>Editorial:${manga.editorial}</h4>
+            <h5>Existencias:${manga.existencias}</h5>
+            <h4>Precio:${manga.precio}</h4>
+            <button>Comprar</button>
+            `;
+            contenidoCatalogo.append(content);  
+            cant++;
+            catalogoVisible.push(content);
+        };
+    });
+    if(cant==0){
+        alert("No hay ningun producto disponible");
+        mostrarCatalogo();
+    }
+}
+function clean(){
+    catalogoVisible.forEach((mangaV)=>{
+        mangaV.remove();
+    })
+}
 function mostrarProductosSeinen(){
     let cant = 0;
+    clean();
     catalogo.forEach((manga) => {
-        if(manga.demografia=='seinen'){
+        if(manga.demografia=='SEINEN'){
             const content = document.createElement("div");
+            content.className+= "tarjeta";
+            content.innerHTML = `  
+            <img src="${manga.img}"> 
+            <h3>${manga.nombre}</h3>
+            <h4>Demografia:${manga.demografia}</h4>
+            <h3>Editorial:${manga.editorial}</h4>
+            <h5>Existencias:${manga.existencias}</h5>
+            <h4>Precio:${manga.precio}</h4>
+            <button>Comprar</button>
+            `;
+            contenidoCatalogo.append(content);  
+            cant++;
+            catalogoVisible.push(content);
+        };
+    });
+    if(cant==0){
+        alert("No hay ningun producto disponible");
+        mostrarCatalogo();
+    }
+}
+function mostrarProductosShojo(){
+    let cant = 0;
+    clean();
+    catalogo.forEach((manga) => {
+        if(manga.demografia=='SHOJO'){
+            const content = document.createElement("div");
+            content.className+= "tarjeta";
             content.innerHTML = `   
             <img src="${manga.img}">
             <h3>${manga.nombre}</h3>
@@ -76,12 +154,41 @@ function mostrarProductosSeinen(){
             `;
             contenidoCatalogo.append(content);  
             cant++;
+            catalogoVisible.push(content);
         };
     });
     if(cant==0){
         alert("No hay ningun producto disponible");
+        mostrarCatalogo();
     }
 }
-console.log(catalogo);
-let palabraPrueba = catalogo[1].nombre.toLowerCase();
-console.log(palabraPrueba[0].toUpperCase());
+function buscador() {
+    clean();
+    let encontrado = false;
+    var buscar = document.getElementById("inputSearch").value;
+    console.log(buscar);
+    manga = catalogo.find(mangaBuscado => {
+    return manga = mangaBuscado.nombre === buscar.toUpperCase()});
+
+    if(manga!=null){
+        encontrado = true;
+        const content = document.createElement("div");
+        content.className+= "tarjeta";
+        content.innerHTML = `   
+        <img src="${manga.img}">
+        <h3>${manga.nombre}</h3>
+        <h4>Demografia:${manga.demografia}</h4>
+        <h3>Editorial:${manga.editorial}</h4>
+        <h5>Existencias:${manga.existencias}</h5>
+        <h4>Precio:${manga.precio}</h4>
+        <div><button>Comprar</button></div>
+        `;
+        contenidoCatalogo.append(content);  
+        catalogoVisible.push(content);
+    }else{
+        alert("El producto solicitado no esta disponible");
+        mostrarCatalogo();
+    }
+}
+
+console.log(catalogoVisible);
